8eda2090bd029e31cec596d20c039ce9317100eb
任亚飞 <dizuo@users.noreply.github.com>
2017-01-03 09:55:26 +0800

    unit test
    test.

--- str/unicode_app.py
+++ str/unicode_app.py
@@ -5,5 +5,13 @@
 def sh_to_us(val):
 	print ( ('\u' + hex(val)[2:]).decode('unicode-escape') )
 
+def unit_test(val):
+	cnt = 4
+	while val < val+cnt:
+		sh_to_us(val)
+		val += 1
+	
 if __name__ == "__main__":
 	sh_to_us(0x4e2d)
+	unit_test()
+	
