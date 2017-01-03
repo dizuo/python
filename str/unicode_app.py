30b811c799abe9788a8b5a8e6f9b899d33850f5b
dizuo <dizuo@126.com>
2017-01-03 10:07:50 +0800

    test

--- str/unicode_app.py
+++ str/unicode_app.py
@@ -5,5 +5,13 @@
 def sh_to_us(val):
 	print ( ('\u' + hex(val)[2:]).decode('unicode-escape') )
 
+def window_unit_test(val, cnt):
+	tmpval = val
+	while val < tmpval + cnt:
+		sh_to_us(val)
+		val += 1
+	
 if __name__ == "__main__":
 	sh_to_us(0x4e2d)
+	window_unit_test(0x4e2d, 4)
+	
